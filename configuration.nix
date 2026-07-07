dba5ba1fb5bdf7e16d324a6162196853b6d37713
TrapNouz <nneaultmadore@gmail.com>
2026-07-06 21:52:08 -0400

    rebuild: 2026-07-06

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs,nix-cachyos-kernel, ... }:
+{ config, pkgs,nix-cachyos-kernel,inputs, ... }:
 
 {
   imports =
@@ -211,6 +211,7 @@ home-manager.users.trapnouz = import ./home.nix;
    obsidian
    protonmail-desktop
    sbctl
+   inputs.helium.packages.${pkgs.system}.default
 
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
--- flake.nix
+++ flake.nix
@@ -4,17 +4,25 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     thyx.url = "github:rccyx/thyx";
+
     home-manager = {                                          
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+      helium = {
+  url = "github:schembriaiden/helium-browser-nix-flake";
+  inputs.nixpkgs.follows = "nixpkgs";
+};
+
     nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
   };
 
-  outputs = { self, nixpkgs, thyx, home-manager, nix-cachyos-kernel }: {
+  outputs = { self, nixpkgs, thyx, home-manager, nix-cachyos-kernel, helium, }@ inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+
       system = "x86_64-linux";
-      specialArgs = { inherit nix-cachyos-kernel; };
+      specialArgs = { inherit inputs;nix-cachyos-kernel = inputs.nix-cachyos-kernel; };
       modules = [
         ./configuration.nix
          home-manager.nixosModules.home-manager
