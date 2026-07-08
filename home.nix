33fa279ca4b46f8a75e43e00a00b4935d7a3ae66
TrapNouz <nneaultmadore@gmail.com>
2026-07-07 20:26:45 -0400

    rebuild: 2026-07-07

--- home.nix
+++ home.nix
@@ -56,7 +56,7 @@
   programs.fish = {
     enable = true;
     shellAliases = {
-      unx = "cd /home/trapnouz/.dotfiles && nix flake update && sudo nixos-rebuild switch --flake /home/trapnouz/.dotfiles#nixos && git add . && git commit -m 'rebuild: '(date +%Y-%m-%d) && git push";
+      unx = "cd /home/trapnouz/.dotfiles && nix flake update && sudo nixos-rebuild switch --flake /home/trapnouz/.dotfiles#nixos && git add . && (git commit -m 'rebuild: '(date +%Y-%m-%d) || true) && git push";
       garbagenix = "nix-collect-garbage -d && sudo nix-collect-garbage -d && nix-store --optimise";
     };
     functions = {
